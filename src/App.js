35e7f34957e5c0e1136fedd013d5fd3d119998ad
shima-hito2 <shimaoka1203@gmail.com>
2024-01-01 22:48:22 +0900

    API作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://locakhost.jp/api");
+      const charaDatas = await res.json();
+      setData(charaDatas.find((data) => (data.id === 1)));
+    })();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.name}
     </div>
   );
 }
