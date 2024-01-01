0c61290a96411d17f0bf812add0d79fe486df771
shima-hito2 <shimaoka1203@gmail.com>
2024-01-01 23:21:38 +0900

    リンク追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import TestA from './TestA.js';
 import TestB from './TestB.js';
 
@@ -22,8 +22,9 @@ function App() {
         <Route exact path='/b' element={<TestB />} />
         {/* <Route path='/login' component={Login} />
       <Route path='/todo' component={TodoList} /> */}
-      </Routes>
-      {/* <Link to='/top'>Back To Top</Link> */}
+      </Routes><br/>
+      <Link to='/'>Aに</Link><br/>
+      <Link to='/b'>Bに</Link>
     </BrowserRouter>
   );
 }
