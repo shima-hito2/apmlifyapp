21b5236a1a61965a5fdf14e1cd7b87ce19797359
shima-hito2 <shimaoka1203@gmail.com>
2024-01-02 16:21:21 +0900

    package.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import TestA from './TestA.js';
 import TestB from './TestB.js';
 
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const res = await fetch("https://locakhost.jp/api");
-      const charaDatas = await res.json();
-      setData(charaDatas.find((data) => (data.id === 2)));
-    })();
-  }, []);
   return (
     <BrowserRouter>
       <Routes>
